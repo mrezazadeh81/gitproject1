88a9821d46c7d59b2f9bc5536ea50a806db755ab
mehdi <rezazadeh838@gmail.com>
2024-03-05 12:02:09 -0800

    p1.py Sum added

--- p1.py
+++ p1.py
@@ -1,5 +1,10 @@
 Factorial = int(input('Enter your number ? '))
-Sum = 1
+
+Sum1 = 1
+Sum2 = 0
+
 for i in range(Factorial, 0, -1):
-    Sum *= i
-print(Sum)
+    Sum1 *= i
+    Sum2 += i
+
+print(Sum1, Sum2)
